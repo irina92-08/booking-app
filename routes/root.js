214193222ab6e9b0f23b5178fe2b67f7e26b180b
Irina Bugrova <irina92-08@mail.ru>
2026-02-04 08:38:24 +0300

    configuring server synchronization with the frontend

--- routes/root.js
+++ routes/root.js
@@ -2,4 +2,7 @@ export default async function (fastify, opts) {
   fastify.get('/', async function (request, reply) {
     return { root: true }
   })
+  fastify.get('/api', async function (request, reply) {
+    return { message: 'привет' };
+  })
 }
